226c4ee0c800e3a08cced02e7bffd47022b19e97
Garaint Anderson <garaintanderson@icloud.com>
2023-08-18 15:02:34 +0100

    Adhere to Bob's Coding Conventions in Maze Class
    Improving the encapsulation and maintainability of the `Maze` class by
    introducing encapsulation principles and adhering to
    Robert C. Martin's (Bob's) coding convention rules. The changes ensure
    a more organized and readable codebase while preserving the integrity
    of the Maze's internal structure.
    
    Improved encapsulation safeguards internal state, reducing
    the risk of unintended interference and promoting a modular design.
    Accessor methods provide controlled access to internal maze data,
    preventing direct manipulation and enhancing code stability.
    Adherence to Bob's coding conventions ensures a consistent and
    standardized codebase, making it easier for developers to understand
    and maintain the `Maze` class.
    The refactor lays the foundation for future modifications and
    feature additions, promoting extensibility and code evolution.

--- code/src/boarder/Maze.java
+++ code/src/boarder/Maze.java
@@ -5,26 +5,34 @@ import javafx.scene.Group;
 import java.util.HashSet;
 import java.util.Set;
 
-// Class maze is a collection of obstacles
+// Class maze is a collection of m_Obstacles
 // This class uses a lot of unnecessary indentation so simplifying this class is needed
 public class Maze {
 
     // Set public member variables to private: Encapsulation
-    private Obstacle obstacle_temp;
-    private Set<Obstacle> obstacles;
+    // Class variables to m_
+    private Set<Obstacle> m_Obstacles;
+
+    // Getters and Setters
+    public Set<Obstacle> getM_Obstacles() {
+        return m_Obstacles;
+    }
+
+    public void setM_Obstacles(Set<Obstacle> m_Obstacles) {
+        this.m_Obstacles = m_Obstacles;
+    }
 
     // Constructor changed to be public for Manager class
     public Maze() {
-        obstacles = new HashSet<>();
-        //initaliseObstacles(); // to initialise all obstacles later used
+        setM_Obstacles(new HashSet<>());
     }
 
     // Function that checks if the current object with coordinates x and y
-    // are touching the hashset of obstacles
+    // are touching the hashset of m_Obstacles
     // Changed 'Boolean' to 'boolean' for primitive boolean variables
     public boolean isTouching(double x, double y, double padding) {
         // For loop checking conditions for collisions across entire rectangle
-        for (Obstacle obstacle :obstacles) {
+        for (Obstacle obstacle : getM_Obstacles()) {
             if (obstacle.isTouching(x, y, padding, obstacle)){
                 return true;
             }
@@ -32,7 +40,7 @@ public class Maze {
         return false;
     }
 
-    // Function that checks if obstacles are touching each other
+    // Function that checks if m_Obstacles are touching each other
     // Renamed parameters to match consistent conventions
     // Deleted variable isTouching, not needed
     public Boolean hasObstacle(double x_from,  double x_to, double y_from, double y_to) {
@@ -46,81 +54,70 @@ public class Maze {
         return false;
     }
 
-    // Function CreateMaze creates multiple objects of obstacles
+    // Function CreateMaze creates multiple objects of m_Obstacles
     // Using the hashset it creates instances of objects and places them in a specified order
     // using magic numbers
     // This function is not appropriate and must be reduced: violates BCC Rule 3
     // It appears to be created a frame around the board and the islands in between
     public void CreateMaze(Group root) {
         //~~~~~~~~~~~~~~~~~~~~~~~~~ frame ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // top
-        this.obstacles.add(new Obstacle(0, 0, "horizontal", 48));
-        // bottom
-        this.obstacles.add(new Obstacle(0, 600, "horizontal", 48));
-        // left
-        this.obstacles.add(new Obstacle(0, 0, "vertical", 25));
-        // right
-        this.obstacles.add(new Obstacle(1225 - Obstacle.getMaxRectangleThickness(), 0, "vertical", 25));
-
+        this.getM_Obstacles().add(new Obstacle(0, 0, "horizontal", 48)); //top
+        this.getM_Obstacles().add(new Obstacle(0, 600, "horizontal", 48)); //bottom
+        this.getM_Obstacles().add(new Obstacle(0, 0, "vertical", 25)); // left
+        this.getM_Obstacles().add(new Obstacle(1225 - Obstacle.getMaxRectangleThickness(), // right
+                0, "vertical", 25));
         //~~~~~~~~~~~~~~~~~~~~~~~~~ Islands ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // obsTopLeft
-        this.obstacles.add(new Obstacle(12 * Obstacle.getMaxRectangleThickness(), Obstacle.getMaxRectangleThickness(), "vertical", 4));
-        // obsTopRight
-        this.obstacles.add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(), Obstacle.getMaxRectangleThickness(), "vertical", 4));
-        // obsBottomLeft
-        this.obstacles.add(new Obstacle(12 * Obstacle.getMaxRectangleThickness(), 600 - 4 * Obstacle.getMaxRectangleThickness(), "vertical", 4));
-        // obsBottomRight
-        this.obstacles.add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(), 600 - 4 * Obstacle.getMaxRectangleThickness(), "vertical", 4));
-        // obsTopMiddle
-        this.obstacles.add(new Obstacle(16 * Obstacle.getMaxRectangleThickness(), 4 * Obstacle.getMaxRectangleThickness(), "horizontal", 17));
-        // obsBottomMiddle
-        this.obstacles.add(new Obstacle(16 * Obstacle.getMaxRectangleThickness(), 600 - 4 * Obstacle.getMaxRectangleThickness(), "horizontal", 17));
-        // obsLMTop
-        this.obstacles.add(new Obstacle(8 * Obstacle.getMaxRectangleThickness(), 8 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // obsLMTop4
-        this.obstacles.add(new Obstacle(8 * Obstacle.getMaxRectangleThickness(), 12 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // obsLMBottom
-        this.obstacles.add(new Obstacle(8 * Obstacle.getMaxRectangleThickness(), 16 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // obsRMTop
-        this.obstacles.add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(), 8 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // obsRMTop2
-        this.obstacles.add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(), 12 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // obsRMBottom
-        this.obstacles.add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(), 16 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // LobsLeftTop1
-        this.obstacles.add(new Obstacle(4 * Obstacle.getMaxRectangleThickness(), 4 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // LobsLeftTop2
-        this.obstacles.add(new Obstacle(4 * Obstacle.getMaxRectangleThickness(), 5 * Obstacle.getMaxRectangleThickness(), "vertical", 6));
-        // LobsLeftBottom1
-        this.obstacles.add(new Obstacle(4 * Obstacle.getMaxRectangleThickness(), 600 - 4 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // LobsLeftBottom2
-        this.obstacles.add(new Obstacle(4 * Obstacle.getMaxRectangleThickness(), 600 - 10 * Obstacle.getMaxRectangleThickness(), "vertical", 6));
-        // LobsRightTop1
-        this.obstacles.add(new Obstacle(40 * Obstacle.getMaxRectangleThickness(), 4 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // LobsRightTop2
-        this.obstacles.add(new Obstacle(44 * Obstacle.getMaxRectangleThickness(), 5 * Obstacle.getMaxRectangleThickness(), "vertical", 6));
-        // LobsRightBottom1
-        this.obstacles.add(new Obstacle(40 * Obstacle.getMaxRectangleThickness(), 600 - 4 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // LobsRightBottom2
-        this.obstacles.add(new Obstacle(44 * Obstacle.getMaxRectangleThickness(), 600 - 10 * Obstacle.getMaxRectangleThickness(), "vertical", 6));
-        // cageBottom
-        this.obstacles.add(new Obstacle(16 * Obstacle.getMaxRectangleThickness(), 600 - 8 * Obstacle.getMaxRectangleThickness(), "horizontal", 17));
-        // cageRightV
-        this.obstacles.add(new Obstacle(32 * Obstacle.getMaxRectangleThickness(), 600 - 16 * Obstacle.getMaxRectangleThickness(), "vertical", 8));
-        // cageLeftV
-        this.obstacles.add(new Obstacle(16 * Obstacle.getMaxRectangleThickness(), 600 - 16 * Obstacle.getMaxRectangleThickness(), "vertical", 8));
-        // cateRightH
-        this.obstacles.add(new Obstacle(17 * Obstacle.getMaxRectangleThickness(), 8 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
-        // cateLeftH
-        this.obstacles.add(new Obstacle(27 * Obstacle.getMaxRectangleThickness(), 8 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(12 * Obstacle.getMaxRectangleThickness(),         // obsTopLeft
+                Obstacle.getMaxRectangleThickness(), "vertical", 4));
+        this.getM_Obstacles().add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(),        // obsTopRight
+                Obstacle.getMaxRectangleThickness(), "vertical", 4));
+        this.getM_Obstacles().add(new Obstacle(12 * Obstacle.getMaxRectangleThickness(),        // obsBottomLeft
+                600 - 4 * Obstacle.getMaxRectangleThickness(), "vertical", 4));
+        this.getM_Obstacles().add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(),        // obsBottomRight
+                600 - 4 * Obstacle.getMaxRectangleThickness(), "vertical", 4));
+        this.getM_Obstacles().add(new Obstacle(16 * Obstacle.getMaxRectangleThickness(),        // obsTopMiddle
+                4 * Obstacle.getMaxRectangleThickness(), "horizontal", 17));
+        this.getM_Obstacles().add(new Obstacle(16 * Obstacle.getMaxRectangleThickness(),        // obsBottomMiddle
+                600 - 4 * Obstacle.getMaxRectangleThickness(), "horizontal", 17));
+        this.getM_Obstacles().add(new Obstacle(8 * Obstacle.getMaxRectangleThickness(),    // obsLMTop
+                8 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(8 * Obstacle.getMaxRectangleThickness(),        // obsLMTop4
+                12 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(8 * Obstacle.getMaxRectangleThickness(),        // obsLMBottom
+                16 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(),        // obsRMTop
+                8 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(),      // obsRMTop2
+                12 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(36 * Obstacle.getMaxRectangleThickness(),      // obsRMBottom
+                16 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(4 * Obstacle.getMaxRectangleThickness(),  // LobsLeftTop1
+                4 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(4 * Obstacle.getMaxRectangleThickness(),// LobsLeftTop2
+                5 * Obstacle.getMaxRectangleThickness(), "vertical", 6));
+        this.getM_Obstacles().add(new Obstacle(4 * Obstacle.getMaxRectangleThickness(),    // LobsLeftBottom1
+                600 - 4 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(4 * Obstacle.getMaxRectangleThickness(),     // LobsLeftBottom2
+                600 - 10 * Obstacle.getMaxRectangleThickness(), "vertical", 6));
+        this.getM_Obstacles().add(new Obstacle(40 * Obstacle.getMaxRectangleThickness(),        // LobsRightTop1
+                4 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(44 * Obstacle.getMaxRectangleThickness(),       // LobsRightTop2
+                5 * Obstacle.getMaxRectangleThickness(), "vertical", 6));
+        this.getM_Obstacles().add(new Obstacle(40 * Obstacle.getMaxRectangleThickness(),    // LobsRightBottom1
+                600 - 4 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(44 * Obstacle.getMaxRectangleThickness(),        // LobsRightBottom2
+                600 - 10 * Obstacle.getMaxRectangleThickness(), "vertical", 6));
+        this.getM_Obstacles().add(new Obstacle(16 * Obstacle.getMaxRectangleThickness(),     // cageBottom
+                600 - 8 * Obstacle.getMaxRectangleThickness(), "horizontal", 17));
+        this.getM_Obstacles().add(new Obstacle(32 * Obstacle.getMaxRectangleThickness(),        // cageRightV
+                600 - 16 * Obstacle.getMaxRectangleThickness(), "vertical", 8));
+        this.getM_Obstacles().add(new Obstacle(16 * Obstacle.getMaxRectangleThickness(),      // cageLeftV
+                600 - 16 * Obstacle.getMaxRectangleThickness(), "vertical", 8));
+        this.getM_Obstacles().add(new Obstacle(17 * Obstacle.getMaxRectangleThickness(),        // cateRightH
+                8 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
+        this.getM_Obstacles().add(new Obstacle(27 * Obstacle.getMaxRectangleThickness(),        // cateLeftH
+                8 * Obstacle.getMaxRectangleThickness(), "horizontal", 5));
 
-        root.getChildren().addAll(obstacles);
-    }
-
-    // Function that adds an obstacle in the specified location
-    // To reduce the long line length of function CreateMaze
-    private void addObstacle(double x, double y, String orientation, double size){
-        // Add the obstacle to the hashset
-        this.obstacles.add(new Obstacle(x, y, orientation, size));
+        root.getChildren().addAll(getM_Obstacles());
     }
 }
